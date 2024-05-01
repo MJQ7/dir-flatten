5d0dfd3b434c21dda988727786025c011d6034ab
Matthew <an@email.address>
2024-05-01 22:11:47 +0100

    Refactor DF class methods in df.py

--- dir_flatten/df.py
+++ dir_flatten/df.py
@@ -2,34 +2,44 @@ import os
 import shutil
 import time
 import dir_flatten.globe
+from dir_flatten.logger import Logger
+from rich import print
 
 class DF():
 
-    LOGGER = None
+    LOGGER = Logger.get()
 
-    def __init__(self, logger):
-        self.LOGGER = logger
-
-    def cleanup_dirs(self):
+    @staticmethod
+    def cleanup_dirs():
         for root, dirs, files in os.walk(dir_flatten.globe.TLDN):
             for dir in dirs:
                 shutil.rmtree(os.path.join(root, dir))  # remove directory
 
+    @staticmethod
     def remove_bad_files(files):
         to_remove = 'Thumbs.db'
         return [file for file in files if to_remove not in file]
 
-    def get_files_in_directory(self, directory):
-        # List to store file names
+    @staticmethod
+    def get_files_in_directory(directory):
+        # List to store file names 
         files = []
-        # Walk through the directory
-        for root, dirs, files_in_dir in os.walk(directory):
-            for file in files_in_dir:
-                files.append(os.path.join(root, file))
+        if not os.path.isdir(directory):
+            print(f"[red]The directory {directory} does not exist.")
+            return []
+        try:
+            # Walk through the directory 
+            for root, files_in_dir in os.walk(directory):
+                for file in files_in_dir:
+                    files.append(os.path.join(root, file))
+        except Exception as e:
+            print(f"[red]An error occurred: {e}")
+            return []
         cleaned = DF.remove_bad_files(files)
         return cleaned
 
-    def move_file_to_top(self, file, dir):
+    @staticmethod
+    def move_file_to_top(file, dir):
         # Get the file name
         file_name = os.path.basename(file)
         # Get the directory name
@@ -37,14 +47,15 @@ class DF():
             # Move the file to the top level directory
             if('header' in file):
                 os.rename(file, os.path.join(dir, 'header_' + file_name))
-                self.LOGGER.write("[yellow]Moved [white]" + file)
+                DF.LOGGER.write("[yellow]Moved [white]" + file)
             else:
                 os.rename(file, os.path.join(dir, file_name))
-                self.LOGGER.write("[green]Moved [white]" + file)
+                DF.LOGGER.write("[green]Moved [white]" + file)
         except FileExistsError:
-            self.LOGGER.write("[yellow]WARN - Already Exists [white]" + file)
+            DF.LOGGER.write("[yellow]WARN - Already Exists [white]" + file)
             os.rename(file, os.path.join(dir, '1_' + file_name))
 
-    def process_file(self, file, dir):
-        DF.move_file_to_top(self, file, dir)
+    @staticmethod
+    def process_file(file, dir):
+        DF.move_file_to_top(file, dir)
         time.sleep(0.1)
