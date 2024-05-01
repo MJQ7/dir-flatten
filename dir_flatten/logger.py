7b1df6183e090e706bf85f26bb08058411b84227
Matthew <an@email.address>
2024-05-01 22:12:29 +0100

    Refactor Logger class methods in logger.py

--- dir_flatten/logger.py
+++ dir_flatten/logger.py
@@ -1,21 +1,28 @@
 from rich.panel import Panel
 
 class Logger():
-
+    _instance = None
     LOG = []
     DISPLAYED_LOG = "\n".join(LOG[-30:])
     LAYOUT = None
 
     @classmethod
-    def create(self, layout):
-        self.LAYOUT = layout
-        Logger.write(self, "[cyan]Starting...")
+    def get(cls):
+        if cls._instance is None:
+            cls._instance = Logger()
+        return cls._instance
 
     @classmethod
-    def update(self):
-        self.DISPLAYED_LOG = "\n".join(Logger.LOG[-30:])
-        self.LAYOUT['log'].update(Panel(self.DISPLAYED_LOG, title="Logs"))
-        
-    def write(self, entry):
-        Logger.LOG.append(entry)
-        Logger.update()
+    def create(cls, layout):
+        cls.LAYOUT = layout
+        cls.write("[cyan]Starting...")
+
+    @classmethod
+    def update(cls):
+        cls.DISPLAYED_LOG = "\n".join(cls.LOG[-30:])
+        cls.LAYOUT['log'].update(Panel(cls.DISPLAYED_LOG, title="Logs"))
+
+    @classmethod
+    def write(cls, entry):
+        cls.LOG.append(entry)
+        cls.update()
